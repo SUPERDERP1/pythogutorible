c95843181b690e77fc596f1601d1648cf0d1f353
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2025-01-08 13:45:21 -0800

    workplz

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("inputForm").addEventListener("submit", (event) => {
     event.preventDefault();
     const inputField = document.getElementById("inputReader")
     const input = inputField.value.trim();
-    
+    console.log("submitted")
 if (input === "help") {
         console.log("success");
 }else{
