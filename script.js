cdecb49a2d2796ba60d0d5dd3837d85f330cc2a3
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2025-01-08 13:45:59 -0800

    RAHHHHHHHHHHHH semicolons

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let screenCounter = 0;
 
 document.getElementById("inputForm").addEventListener("submit", (event) => {
     event.preventDefault();
-    const inputField = document.getElementById("inputReader")
+    const inputField = document.getElementById("inputReader");
     const input = inputField.value.trim();
-    console.log("submitted")
+    console.log("submitted");
 if (input === "help") {
         console.log("success");
 }else{
