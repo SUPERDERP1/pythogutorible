3fc981d7f3dc99dcda8fc9397a41ae780ea63a4b
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2025-01-08 16:56:14 -0800

    fix maybe rahhhhh

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let screenCounter = 0;
+document.addEventListener("documentLoaderbecauseeventlistenersarestupid", () => {
+    let screenCounter = 0;
 
-document.getElementById("inputForm").addEventListener("submit", (event) => {
-    event.preventDefault();
-    const inputField = document.getElementById("inputReader");
-    const input = inputField.value.trim();
-    console.log("submitted");
-if (input === "help") {
-    console.log("success");
-}else{
-    console.error("invalid");
-}
+    document.getElementById("inputForm").addEventListener("submit", (event) => {
+        event.preventDefault();
+        const inputField = document.getElementById("inputReader");
+        const input = inputField.value.trim();
+        console.log("submitted");
+        if (input === "help") {
+            console.log("success");
+        } else {
+            console.error("invalid");
+        }
+    });
 });
-
