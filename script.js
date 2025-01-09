9b12dd8d7d11cfa1ad947248bd1436412d5e650c
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2025-01-09 11:59:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // js code sucks
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM loaded");
+let screens = 0;
+document.addEventListener("DOMContentLoaded", () => { // why does the whole program kill itself if this isnt here
+    console.log("the thing is working");
 
     document.getElementById("inputForm").addEventListener("submit", (event) => {
         event.preventDefault();
@@ -15,12 +16,26 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log("success");
             errorMessage.style.color = "green";
             errorMessage.textContent = "Success! Next up: placeholder";
+            screens += 1;
             setTimeout(() => {
                 window.location.href = "index2.html"; 
             }, 3000); // in ms btw
+
+        } else if (input === "help" && screens == 1){
+            console.log("successScr2");
+            errorMessage.style.color = "green";
+            errorMessage.textContent = "Success! Next up: placeholder";
+            screens += 1;
+            setTimeout(() => {
+                window.location.href = "index2.html"; 
+            }, 3000);// still ms
+            
+            
         } else {
-            console.error("invalid");
+            console.error("invalidScr");
             errorMessage.textContent = "Not Quite! Try Again";
-        }
+            setTimeout(() => {
+                errormessage.textContent = "";
+            }, 3750); // still still ms
     });
 });
