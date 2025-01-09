20aa85254c5c27755447392934869fbde57fce23
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2025-01-08 17:08:48 -0800

    fixie wixies

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-document.addEventListener("documentLoaderbecauseeventlistenersarestupid", () => {
+document.addEventListener("DOMContentLoaded", () => {
     let screenCounter = 0;
 
     document.getElementById("inputForm").addEventListener("submit", (event) => {
         event.preventDefault();
         const inputField = document.getElementById("inputReader");
         const input = inputField.value.trim();
+       
         console.log("submitted");
+       
         if (input === "help") {
             console.log("success");
         } else {
             console.error("invalid");
         }
+
+        
     });
 });
+       
