c02fa5db95e9b04d37da973ced061150a8e3ecc5
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2025-01-08 17:26:54 -0800

    BIG additions

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+// js code sucks
 document.addEventListener("DOMContentLoaded", () => {
-    let screenCounter = 0;
+    console.log("DOM loaded");
 
     document.getElementById("inputForm").addEventListener("submit", (event) => {
         event.preventDefault();
         const inputField = document.getElementById("inputReader");
         const input = inputField.value.trim();
+        const errorMessage = document.getElementById("errorMessage");
        
         console.log("submitted");
        
         if (input === "help") {
             console.log("success");
-            window.location.href = "index2.html"; 
+            errorMessage.color = "green";
+            errorMessage.textContent = "Success! Next up: placeholder";
+            setTimeout(() => {
+                window.location.href = "index2.html"; 
+            }, 3000); // in ms btw
         } else {
             console.error("invalid");
+            errorMessage.textContent = "Not Quite! Try Again";
         }
     });
 });
