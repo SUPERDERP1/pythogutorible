532504ead01649a9c3da393c7c1016afc422abb3
Jett Gentes <146028226+SUPERDERP1@users.noreply.github.com>
2025-01-13 14:56:04 -0800

    help

--- script.js
+++ script.js
@@ -9,16 +9,14 @@ console.log("submitted");
         
 console.log("success");
 //errorMessage.style.color = "green";
-errorMessage.textContent = "Success! Next up: placeholder";
-screens += 1;
+
 setTimeout(() => {
     window.location.href = "index2.html"; 
 }, 3000); // in ms btw
 
       
 console.log("successScr2");
-//errorMessage.style.color = "green";
-errorMessage.textContent = "Success! Next up: placeholder";
+
 screens += 1;
 setTimeout(() => {
     window.location.href = "index2.html"; 
